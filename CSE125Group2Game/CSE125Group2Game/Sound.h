b4489ad9a6b57a964ff0d26d39dce2179ae2f863
Alex84708 <agarza@ucsd.edu>
2021-05-12 19:32:29 -0700

    Added sound deconstructor

--- CSE125Group2Game/CSE125Group2Game/Sound.h
+++ CSE125Group2Game/CSE125Group2Game/Sound.h
@@ -1,34 +1,36 @@
-#pragma once
+﻿#pragma once
 
 #include <conio.h>
-
+#include <irrKlang.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <string>
-#include <irrKlang.h>
 
 #include "GameObject.h"
 
-#pragma comment(lib, "irrKlang.lib") // link with irrKlang.dll
+#pragma comment(lib, "irrKlang.lib")  // link with irrKlang.dll
 
 class Sound {
-public:
-	const char* shootingSoundPath = "../Shared/Assets/Sounds/laser.mp3";
+ public:
+  const char* shootingSoundPath = "../Shared/Assets/Sounds/laser.mp3";
 
-	Sound();
-	void play(const char* filePath, irrklang::vec3df positionOfSoundOrigin);
+  Sound();
+  void play(const char* filePath, irrklang::vec3df positionOfSoundOrigin);
 
-	/*
-		if toPause is true, it will pause all sounds
-		else, it will unpause all sounds
-	*/
-	void pauseAllSounds(bool toPause);
+  /*
+          if toPause is true, it will pause all sounds
+          else, it will unpause all sounds
+  */
+  void pauseAllSounds(bool toPause);
 
-	void stopAllSounds();
-	void setListenerPosition(Transform* playerTransform);
-	void playAccordingToGameObject(GameObject* obj);
+  void stopAllSounds();
+  void setListenerPosition(Transform* playerTransform);
+  void playAccordingToGameObject(GameObject* obj);
 
-private:
-	irrklang::ISoundEngine* engine;
-	std::vector<std::string> mSoundVector;
+  ~Sound() { delete engine; }
+
+ private:
+  irrklang::ISoundEngine* engine;
+  std::vector<std::string> mSoundVector;
 };
